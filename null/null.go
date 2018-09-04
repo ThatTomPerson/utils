f0387da0c2cbbaec609848e7890bc92fa4f333a1
Thomas Albrighton <thomasalbrighton@gmail.com>
2018-09-04 12:17:47 +0930

    Added docs for null functions

--- null/null.go
+++ null/null.go
@@ -1,24 +1,32 @@
+// Package null reduces some boilerplate when working with sql.Null types
 package null
 
 import (
 	"database/sql"
 )
 
+// String returns a sql.NullString
 func String(s interface{}) sql.NullString {
 	var v sql.NullString
 	v.Scan(s)
 	return v
 }
+
+// Int returns a sql.NullInt64
 func Int(s interface{}) sql.NullInt64 {
 	var v sql.NullInt64
 	v.Scan(s)
 	return v
 }
+
+// Float returns a sql.NullFloat64
 func Float(s interface{}) sql.NullFloat64 {
 	var v sql.NullFloat64
 	v.Scan(s)
 	return v
 }
+
+// Bool returns a sql.NullBool
 func Bool(s interface{}) sql.NullBool {
 	var v sql.NullBool
 	v.Scan(s)
