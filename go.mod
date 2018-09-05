1179cb07bb984c8a04b3f9aa85a8fde825bce4d3
Thomas Albrighton <thomasalbrighton@gmail.com>
2018-09-05 10:05:54 +0930

    added aws db helper

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
 module github.com/ThatTomPerson/utils
 
 require (
+	github.com/aws/aws-sdk-go v1.15.27
+	github.com/aws/aws-xray-sdk-go v0.9.4
+	github.com/cihub/seelog v0.0.0-20170130134532-f561c5e57575 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/davecheney/godoc2md v0.0.0-20180717000503-586c20adbfb9 // indirect
+	github.com/pkg/errors v0.8.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
 	golang.org/x/tools v0.0.0-20180831211245-7ca132754999 // indirect
---
+++ smdb/smdb.go
@@ -0,0 +1,51 @@
+package smdb
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+	"github.com/aws/aws-xray-sdk-go/xray"
+)
+
+type Creds struct {
+	Username            string `json:"username"`
+	Password            string `json:"password"`
+	Engine              string `json:"engine"`
+	Host                string `json:"host"`
+	Port                int    `json:"port"`
+	DbClusterIdentifier string `json:"dbClusterIdentifier"`
+}
+
+func (c *Creds) DSL(database string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?parseTime=true&sql_mode=ansi", c.Username, c.Password, c.Host, c.Port, database)
+}
+
+func New(secret, database string) (*xray.DB, error) {
+	creds, err := GetCreds(secret)
+	if err != nil {
+		return nil, err
+	}
+
+	return xray.SQL(creds.Engine, creds.DSL(database))
+}
+
+func GetCreds(secret string) (*Creds, error) {
+	svc := secretsmanager.New(session.New())
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: aws.String(secret),
+	}
+
+	result, err := svc.GetSecretValue(input)
+
+	if err != nil {
+		return nil, err
+	}
+	var c Creds
+
+	err = json.Unmarshal([]byte(*result.SecretString), &c)
+
+	return &c, err
+}
